41f1b0b6323dc4b7581ae1e2420843fdf5d586b5
Gema <gem.anggada@gmail.com>
2020-10-18 16:25:54 +0700

    just made Nunito to be present as default

--- @narative/gatsby-theme-novela/src/styles/global.ts
+++ @narative/gatsby-theme-novela/src/styles/global.ts
@@ -44,7 +44,7 @@ export const globalStyles = css`
   }
 
   body {
-    font-family: '-apple-system', 'BlinkMacSystemFont', 'San Francisco',
+    font-family: Nunito, '-apple-system', 'BlinkMacSystemFont', 'San Francisco',
       'Helvetica Neue', 'Helvetica', 'Ubuntu', 'Roboto', 'Noto', 'Segoe UI',
       'Arial', sans-serif;
     font-size: 1.6rem;
@@ -81,7 +81,7 @@ export const globalStyles = css`
   textarea,
   select,
   button {
-    font-family: '-apple-system', 'BlinkMacSystemFont', 'San Francisco',
+    font-family: Nunito, '-apple-system', 'BlinkMacSystemFont', 'San Francisco',
       'Helvetica Neue', 'Helvetica', 'Ubuntu', 'Roboto', 'Noto', 'Segoe UI',
       'Arial', sans-serif;
   }

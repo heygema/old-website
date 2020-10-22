a2e69605265dadd04fa5843be7dd6534b20cf420
Gema <gem.anggada@gmail.com>
2020-10-22 15:07:08 +0700

    tag.template uses its own article list container

--- @narative/gatsby-theme-novela/src/templates/tag.template.tsx
+++ @narative/gatsby-theme-novela/src/templates/tag.template.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+import Headings from '@components/Headings';
 import Section from '@components/Section';
 import SEO from '@components/SEO';
 import Layout from '@components/Layout';
 import Paginator from '@components/Navigation/Navigation.Paginator';
-
-import AuthorArticles from '../sections/author/Author.Articles';
+import mediaqueries from '@styles/media';
+import ArticlesList from '../sections/articles/Articles.List.tsx';
 
 function TagPage({ location, pageContext }) {
   const { group: articles, tag } = pageContext;
 
-  console.log('(TagPage) tag ?', tag);
-
   return (
     <Layout>
       <SEO pathname={location.pathname} title={tag} />
+      <Section>
+        <HeroHeading>{`All articles tagged in #${tag}`}</HeroHeading>
+      </Section>
       <Section narrow>
-        <AuthorArticles articles={articles} tags={true} />
+        <ArticlesContainer>
+          <ArticlesList articles={articles} alwaysShowAllDetails />
+        </ArticlesContainer>
+
         <AuthorPaginator>
           <Paginator {...pageContext} />
         </AuthorPaginator>
@@ -28,6 +33,39 @@ function TagPage({ location, pageContext }) {
 }
 
 export default TagPage;
+
+const ArticlesContainer = styled.div`
+  border-radius: 8px;
+  padding: 88px 98px;
+  position: relative;
+  z-index: 1;
+
+  ${mediaqueries.desktop_medium`
+    padding: 80px;
+  `}
+
+  ${mediaqueries.desktop`
+    padding: 0;
+    background: transparent;
+  `}
+`;
+
+const HeroHeading = styled(Headings.h2)`
+  font-family: ${(p) => p.theme.fonts.serif};
+  margin-top: 60px;
+  margin-bottom: 25px;
+  font-weight: bold;
+  line-height: 1.32;
+
+  ${mediaqueries.tablet`
+    margin-bottom: 20px;
+    font-size: 3rem;
+  `}
+
+  ${mediaqueries.phablet`
+    font-size: 3rem;
+  `}
+`;
 
 const AuthorsGradient = styled.div`
   position: absolute;

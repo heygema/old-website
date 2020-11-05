77427d7d01491fda1fb1afc4e1d786fa6fc54b2c
Gema <gem.anggada@gmail.com>
2020-11-05 15:20:54 +0700

    fix(gooey): removed unused import

--- @narative/gatsby-theme-novela/src/components/Gooey/Gooey.tsx
+++ @narative/gatsby-theme-novela/src/components/Gooey/Gooey.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { useColorMode } from 'theme-ui';
 
 import Image from '@components/Image';
 import mediaqueries from '@styles/media';
